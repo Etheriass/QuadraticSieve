7d59f47d5ec65e5e1d7fdd46dd0d38b860b0ea1e
Etheriass <funckialexjoly@gmail.com>
2025-01-19 12:35:26 -0600

    Update benchmark

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <chrono>
+#include <math.h>
 #include "../src/Eratosthene/eratosthene.hpp"
+#include "../src/Friables/friables.hpp"
 
 
 void benchmark(){
     auto start = std::chrono::high_resolution_clock::now();
 
-    int N = 1000000;
+    unsigned long long N = 184467440737095601;
 
-    std::vector<int> primes = eratostheneSieve(N);
+    // std::vector<int> primes = eratostheneSieve(N);
+    int B = (int)exp(0.5*sqrt(log(N) * log(log(N))));
+    int A = 1002000;
+    int nbQf;
+    unsigned long long *Qf = QBfriablesV2Long(B, N, A, &nbQf);
+    std::cout << nbQf << std::endl;
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
