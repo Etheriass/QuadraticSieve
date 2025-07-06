0bf3fabb48a01b766642bc699047f99b03dd3bac
Etherias <funckialexjoly@gmail.com>
2025-07-06 18:43:50 -0400

    Remove DAC legacy

--- src/DAC/dac_multiplication_alpha.h
+++
@@ -1,6 +0,0 @@
-#ifndef DAC_MULTIPLICATION_ALPHA_H
-#define DAC_MULTIPLICATION_ALPHA_H
-
-int **dac_square_mat_product_alpha(int **A, int **B, int n, int deallocate, int threshold);
-
-#endif
--- src/DAC/dac_multiplication_strassen_parra.h
+++
@@ -1,6 +0,0 @@
-#ifndef DAC_MULTIPLICATION_STRASSEN_PARRA_H
-#define DAC_MULTIPLICATION_STRASSEN_PARRA_H
-
-int **dac_square_mat_product_strassen_parra(int **A, int **B, int n, int deallocate);
-
-#endif
